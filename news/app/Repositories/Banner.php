5b146ab28d59e6e74cfe60f00527b7bfbf1479d1
LiChunHua <lichunhua@medatc.com>
2018-01-24 11:31:01 +0800

    x

---
+++ news/app/Repositories/Banner.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Repositories;
+
+class Banner
+{
+    public function all($areaId)
+    {
+        $config = app('db')->table('site_setting')
+            ->select('value')
+            ->where('site_id', $areaId)
+            ->where('path', 'app.news.banner.top')
+            ->value('value');
+
+        $config = json_decode($config, true);
+        $items = $config['childrens'] ?? [];
+
+        return array_map(function ($item) {
+            $item['news_id'] = trim($item['url'], '#');
+            unset($item['url']);
+            return $item;
+        }, $items);
+    }
+}
