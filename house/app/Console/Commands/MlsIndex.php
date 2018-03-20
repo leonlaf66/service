ba6d00f45f954567f08134ec41cd2d1c1363f5ac
LiChunHua <lichunhua@medatc.com>
2018-03-20 17:39:50 +0800

    x

--- house/app/Console/Commands/MlsIndex.php
+++ house/app/Console/Commands/MlsIndex.php
@@ -14,7 +14,7 @@ class MlsIndex extends Command
 
         $query = app('db')->connection('pgsql2')
             ->table('mls_rets')
-            ->select('update_date', 'json_data')
+            ->select('update_date', 'est_sale', 'estimation', 'json_data')
             ->orderBy('list_no');
 
         if ($mode === 'new') {
