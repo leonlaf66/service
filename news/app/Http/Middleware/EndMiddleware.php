91df17807881024133116cba82b242dcdea943a5
LiChunHua <lichunhua@medatc.com>
2018-05-31 02:24:14 +0800

    x

---
+++ news/app/Http/Middleware/EndMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class EndMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $result = $next($request);
+        app('db')->disconnect();
+        return $result;
+    }
+}
