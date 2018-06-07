e9b0dce7e9e3817a25bf1e36c87f1fe876ed9dfc
LiChunHua <lichunhua@medatc.com>
2018-06-07 19:17:37 +0800

    x

--- house-srv/server/index.js
+++ house-srv/server/index.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const { Connection } = require('socket-json-wrapper')
 const config = require('./config');
 const HOST = '0';
 const PORT = config.server.port;
@@ -6,22 +7,25 @@ const PORT = config.server.port;
 var clients = {};
 net.createServer(sock => {
   sock.setEncoding('utf8');
-  sock.on('data', data => {
+
+  const connection = new Connection(sock);
+  connection.on('message', data => {
     try {
       // 解析数据
       data = JSON.parse(data);
+      
       // 反馈
-      sock.write(JSON.stringify({
+      connection.send({
         id: data.id,
         status: true
-      }) + "\n");
+      });
+
       //打印输出
       console.log(data.id);
     } catch (e) {
       console.log('Error: ' + data);
     }
   });
-
 }).listen(PORT, HOST);
 
 console.log('Server listening on ' + HOST +':'+ PORT);
