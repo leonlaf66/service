54e9f492a73bd17ebb4be3f797678e3a1969bc1a
LiChunHua <lichunhua@medatc.com>
2018-08-28 11:34:48 +0800

    x

--- graphql-apis/graphql/@local-modules/house-builder.js
+++ graphql-apis/graphql/@local-modules/house-builder.js
@@ -5,6 +5,7 @@ export default (areaId) => {
     .select('*')
     .column({id: 'list_no'})
     .where('area_id', areaId)
+    .where('city_id', '>', 0)
     .where('is_online_abled', true)
     .orderBy('list_date', 'desc')
 }
